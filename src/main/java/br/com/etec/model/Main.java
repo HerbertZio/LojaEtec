a20e2969dbeca83c804745ff316083eec57a64d9
Herbert <herbertpzio@gmail.com>
2024-08-19 20:30:20 -0300

    Segundo Commit

--- src/main/java/br/com/etec/model/Main.java
+++ src/main/java/br/com/etec/model/Main.java
@@ -4,15 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/br/com/etec/view/tela-inicial.fxml"));
-        Scene scene = new Scene(loader.load(),300,100);
+        Scene scene = new Scene(loader.load(),600,600);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Abrir Breguenaite!");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
