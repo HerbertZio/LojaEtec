de09d699caaa5ce93624bd1ac441ca8863103ba7
Herbert <herbertpzio@gmail.com>
2024-09-02 20:42:46 -0300

    02-09-2024

--- src/main/java/br/com/etec/controller/Operacoes.java
+++ src/main/java/br/com/etec/controller/Operacoes.java
@@ -1,12 +1,11 @@
 package br.com.etec.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
 
 public class Operacoes {
 
@@ -21,17 +20,25 @@ public class Operacoes {
 
     @FXML
     private void validarUsuario(ActionEvent event) {
-        String nomeUsuario;
-        nomeUsuario = txfUsuario.getText();
-        String senhaUsuario;
-        senhaUsuario = psfSenha.getText();
+        System.out.println("Método validarUsuario chamado");
+        String nomeUsuario = txfUsuario.getText();
+        String senhaUsuario = psfSenha.getText();
 
-        if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()){
-            if(nomeUsuario.isEmpty()){
-                //nome vazio
-            } else {
-                // Senha vazia
-            }
+        if (nomeUsuario.isEmpty()) {
+            mostrarAlerta(Alert.AlertType.WARNING, "Informe o nome!", "Você precisa inserir o nome!");
+        } else if (senhaUsuario.isEmpty()) {
+            mostrarAlerta(Alert.AlertType.WARNING, "Informe a senha!", "Você precisa inserir a senha!");
+        } else if (nomeUsuario.equals("admin") && senhaUsuario.equals("123456")) {
+            mostrarAlerta(Alert.AlertType.CONFIRMATION, "Acesso permitido!", "Login bem sucedido!");
+        } else {
+            mostrarAlerta(Alert.AlertType.ERROR, "Acesso Negado!", "Usuário ou senha inválidos");
         }
+    }
+
+    private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensagem) {
+        Alert alerta = new Alert(tipo);
+        alerta.setTitle(titulo);
+        alerta.setContentText(mensagem);
+        alerta.showAndWait();
     }
 }
